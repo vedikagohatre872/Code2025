a8c9d57ab70be50f51ca5370539f4b46205f0227
vedikagohatre872 <vedikagohatre872@gmail.com>
2025-08-15 19:52:00 +0530

    Update Day 1.C

--- Day 1.C
+++ Day 1.C
@@ -1,1 +1,12 @@
+Sort an Array of 0s, 1s, and 2s
+
+You are given an array arr consisting only of 0s, 1s, and 2s. The task is to sort the array in increasing order in linear time (i.e., O(n)) without using any extra space. This means you need to rearrange the array in-place.
+
+Input:
+An integer array arr of size n where each element is either 0, 1, or 2.
+Example : arr = [0, 1, 2, 1, 0, 2, 1, 0]
+
+Output:
+The sorted array, arranged in increasing order of 0s, 1s, and 2s.
+Example: [0, 0, 0, 1, 1, 1, 2, 2]
 
